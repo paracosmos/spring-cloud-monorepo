41963acf550ee8cd854af3ae284cdaa6123e9f23
김형준(케이드) <iwillbebback@gmail.com>
2025-03-12 17:43:25 +0900

    update user entity

--- user/src/main/kotlin/com/skdnd/user/controller/UserController.kt
+++ user/src/main/kotlin/com/skdnd/user/controller/UserController.kt
@@ -2,8 +2,9 @@ package com.skdnd.user.controller
 
 import com.skdnd.user.entity.User
 import com.skdnd.user.service.UserService
-import org.springframework.web.bind.annotation.*
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
@@ -18,16 +19,19 @@ class UserController(
   }
 
   @GetMapping("/{id}")
-  suspend fun getUserById(@PathVariable id: Int): User {
-    return userService.getUserById(id) ?: throw ResponseStatusException(
-      HttpStatus.NOT_FOUND, "User not found"
-    )
+  suspend fun getUserById(@PathVariable id: Int): ResponseEntity<User> {
+    val user = userService.getUserById(id)
+      ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "User not found")
+    return ResponseEntity.ok(user)
   }
 
   @PostMapping
-  suspend fun addUser(@RequestBody request: Map<String, String>): Int {
-    val name = request["name"] ?: throw IllegalArgumentException("Name is required")
-    val password = request["password"] ?: throw IllegalArgumentException("Password is required")
-    return userService.addUser(name, password)
+  suspend fun addUser(@RequestBody request: Map<String, String>): ResponseEntity<Map<String, Int>> {
+    val name = request["name"] ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Name is required")
+    val password = request["password"] ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Password is required")
+    val id = userService.addUser(name, password)
+    return ResponseEntity.status(HttpStatus.CREATED)
+      .body(mapOf("id" to id))
   }
+
 }
--- user/src/main/kotlin/com/skdnd/user/entity/User.kt
+++ user/src/main/kotlin/com/skdnd/user/entity/User.kt
@@ -1,8 +1,10 @@
 package com.skdnd.user.entity
 
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Table
-
+//import org.jetbrains.exposed.sql.Table
 /*
 #DDL
 CREATE TABLE users (
@@ -10,11 +12,18 @@ CREATE TABLE users (
     name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
- */
+*/
 
 object Users : IntIdTable("users") {
     val name = varchar("name", 255)
     val password = varchar("password", 255)
+}
+
+class UserEntity(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<UserEntity>(Users)
+    var name by Users.name
+    var password by Users.password
+    fun toDomain() = User(id.value, name, password)
 }
 
 data class User(
